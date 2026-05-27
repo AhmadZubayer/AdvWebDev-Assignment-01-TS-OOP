33b82699389a00bcc7c642ee3fe834852da394be
AhmadZubayer <ahmadzubayer369@gmail.com>
2026-05-27 22:08:39 +0600

    solved problem 6 7

--- solutions.ts
+++ solutions.ts
@@ -41,11 +41,56 @@ interface Book {
     publishedYear : number;
 }
 
-const toggleReadStatus = <Book>(bookInfo : Book) => {
-    return {
-        ...bookInfo,
-        isRead: true
-    };
-}
+const toggleReadStatus = (bookInfo: Book) => {
+  return {
+    ...bookInfo,
+    isRead: true
+  };
+};
+
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
 console.log(toggleReadStatus(myBook));
+
+// PROBLEM 06
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age); 
+    this.grade = grade;
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
+}
+
+
+const student = new Student("Alice", 20, "A");
+const result = student.getDetails();
+
+console.log(result); 
+
+// PROBLEM 07
+const getIntersection = (arr1: number[], arr2: number[]) => {
+  let newArr : number[] = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr2.includes(arr1[i])) {
+      newArr.push(arr1[i]);
+    }
+  }
+  return newArr;
+}
+
+const result7 = getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]);
+console.log(result7);
